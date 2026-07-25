ff2abd2dc2a342d22404c9bf14953f9c3b3bb87d
thetopham <124335168+thetopham@users.noreply.github.com>
2026-07-25 17:14:03 -0600

    revert: show minimap in roleplay

--- packages/client/src/components/game/GameMap.tsx
+++ packages/client/src/components/game/GameMap.tsx
@@ -749,7 +749,7 @@ export function GameMapPanel({
           {collapsed ? <ChevronDown size={12} /> : <ChevronUp size={12} />}
         </span>
       </div>
-      {!collapsed && hasWorldMap && map && (
+      {!collapsed && hasWorldMap && (
         <GameMapViewTabs value={effectiveMapView} onChange={setMapViewMode} />
       )}
       {!collapsed && effectiveMapView === "local" && mapOptions.length > 1 && (
