e58cbb2452b950345cb1b92057baee9a6dae7c02
Spicy_Marinara <mgrabower97@gmail.com>
2026-08-01 02:07:44 +0200

    fix Professor Mari bulk delete accent

--- packages/client/src/components/chat/HomeProfessorMariChat.tsx
+++ packages/client/src/components/chat/HomeProfessorMariChat.tsx
@@ -3809,7 +3809,7 @@ export function HomeProfessorMariChat({
                             type="button"
                             onClick={() => void handleBulkDeleteProfessorChats()}
                             disabled={selectedChatHistoryIds.size === 0}
-                            className="mari-chrome-control mari-chrome-control--danger mari-chrome-control--small h-8 px-3 text-[0.625rem]"
+                            className="mari-chrome-control mari-chrome-control--primary mari-chrome-control--small h-8 px-3 text-[0.625rem]"
                           >
                             <Trash2 size="0.75rem" />
                             {localizeUi("ui.chat.homeprofessormarichat.deleteSelectedChats")}
